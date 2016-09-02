783af801a52184099bfc6eed3b3faf0244371eed
roger.pence <roger.pence@gmail.com>
2016-09-01 22:34:07 -0500

    make a post with JavaScript

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,33 @@
 var rp = rp || {};
 
-rp.main = function() {
+rp.main = function () {
 
+    function postGeoLocation() {
+        var data = {
+            "routeid": 34567,
+            "latitude": "29.6537893",
+            "longitude": "-91.5678932",
+            "captured_at": "2016-08-31T21:01:47.894512"
+        };
 
+        var options = {
+            type: 'POST',
+            url: '/Foo',
+            data: JSON.stringify(data),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json'
+        };
 
+        $.ajax(options).
+            done((data) => {
+                var j = data;
+            }).
+            fail((error) => {
+
+            })
+    }
 
     return {
-
+        postGeoLocation: postGeoLocation
     };
-}();
+} ();
---
+++ static/js/geowatcher.js
@@ -0,0 +1,73 @@
+var rp = rp || {};
+
+rp.getGeoLocationManager = function() {
+    var options;
+    const MOVEMENT_FACTOR = .0145;
+    const DUMMY_START_LATITUDE = 28.5;
+    const DUMMY_START_LONGITUDE = -98.5;
+
+    var geoPosition = {
+        latitude: 0,
+        longitude: 0,
+        previousLatitude: 0,
+        previousLongitude: 0,
+        timeCaptured: ''        
+    }
+
+    function captureGeoLocation() {
+        navigator.geolocation.getCurrentPosition(reportCurrentPosition, geoError);
+        // Dummy won't need HTTPS.
+        //dummyGetCurrentPosition(reportCurrentPosition, geoError)                
+    }
+
+    function geoError(positionError) {
+        // Handle GEO error here. 
+        var x = positionError;
+        console.log("GEO error occured.")
+    }
+
+    function dummyGetCurrentPosition(success, error) {
+        success(geoPosition);
+    }
+
+    function reportCurrentPosition(position) {
+        if (this.options.emulateMovement) {            
+            geoPosition.latitude = (geoPosition.latitude == 0) ? Number.parseFloat(position.coords.latitude.toFixed(7)) : geoPosition.latitude  + MOVEMENT_FACTOR;  
+            geoPosition.longitude = (geoPosition.longitude == 0) ? Number.parseFloat(position.coords.longitude.toFixed(7)) : geoPosition.longitude + MOVEMENT_FACTOR; 
+        }                
+        else {
+            geoPosition.latitude = Number.parseFloat(position.coords.latitude.toFixed(7));
+            geoPosition.longitude = Number.parseFloat(position.coords.longitude.toFixed(7));
+        }
+
+        geoPosition.timeCaptured = new Date(position.timestamp).toISOString();
+
+        this.options.captureCallback(geoPosition);
+
+        geoPosition.previousLatitude = geoPosition.latitude;
+        geoPosition.previousLongitude = geoPosition.longitude;
+    }
+
+    function startGeoLocationCapture(options) {
+        this.options = options;
+        options.captureSecondsInterval = this.options.captureSecondsInterval * 1000;
+        geoPosition.latitude = 0;
+        geoPosition.longitude = 0;
+        geoPosition.previousLatitude = 0;
+        geoPosition.previousLongitude = 0;
+
+        if (navigator.geolocation) {
+            captureGeoLocation();
+            options.captureId = setInterval(function() {
+                captureGeoLocation();
+            }, options.captureSecondsInterval);
+        }
+        else {
+            console.log("Geolocation is not supported by this browser");
+        }
+
+    };      
+    return {
+        startGeoLocationCapture: startGeoLocationCapture
+    }                     
+}();
